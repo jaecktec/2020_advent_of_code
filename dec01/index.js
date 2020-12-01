88fd06cd90bddf722bf776063734b57da6d0e512
Constantin <jaecktec@users.noreply.github.com>
2020-12-01 10:26:52 +0100

    Add unneccisary performance optimization

--- dec01/index.js
+++ dec01/index.js
@@ -1,31 +1,42 @@
+const { performance, PerformanceObserver } = require('perf_hooks');
+const obs = new PerformanceObserver((items) => {
+    items.getEntries().forEach(({duration, name}) => {
+        console.log(`${name} = ${duration} ms`)
+    });
+    // performance.clearMarks();
+});
+obs.observe({ entryTypes: ['measure'] });
+performance.measure('Start to Now');
 const values = require('./values.json');
 
-console.log(values);
 const array = values.values
 
 
-function calculateStep1(){
+function calculateStep1() {
     for (let a = 0; a < array.length; a++) {
         const ra = array[a];
         for (let b = 0; b < array.length; b++) {
+            if(a < b) continue;
             const rb = array[b];
-            if(ra + rb === 2020){
-                console.log(`${ra} + ${rb} === 2020, => ${ra} * ${rb} = ${ra*rb}`);
+            if (ra + rb === 2020) {
+                console.log(`${ra} + ${rb} === 2020, => ${ra} * ${rb} = ${ra * rb}`);
                 return;
             }
         }
     }
 }
 
-function calculateStep2(){
+function calculateStep2() {
     for (let a = 0; a < array.length; a++) {
         const ra = array[a];
         for (let b = 0; b < array.length; b++) {
             const rb = array[b];
+            if(a < b) continue;
             for (let c = 0; c < array.length; c++) {
+                if(b < c) continue;
                 const rc = array[c];
-                if(ra + rb + rc === 2020){
-                    console.log(`${ra} + ${rb} + ${rc} === 2020, => ${ra} * ${rb} * ${rc} = ${ra*rb*rc}`);
+                if (ra + rb + rc === 2020) {
+                    console.log(`${ra} + ${rb} + ${rc} === 2020, => ${ra} * ${rb} * ${rc} = ${ra * rb * rc}`);
                     return;
                 }
             }
@@ -33,5 +44,10 @@ function calculateStep2(){
     }
 }
 
+performance.mark('A');
 calculateStep1();
+performance.mark('B');
 calculateStep2();
+performance.mark('C');
+performance.measure('A to B', 'A', 'B');
+performance.measure('B to C', 'B', 'C');
